54da3aa3ac4718a5d8caab1488f710f6a5889c79
dw3105 <dev.zaigraev@gmail.com>
2018-08-16 01:15:18 +0400

    Updated wrappers/pfp_wrapper.sh

--- wrappers/pfp_wrapper.sh
+++ wrappers/pfp_wrapper.sh
@@ -1,10 +1,11 @@
 #!/bin/ksh
 
 export JAVA_1_8_HOME=/usr/java8_64/bin
+export PFP_HOME=/home/pfp
 
 function pfp { 
 	set -f
 	typeset params="$*"
-	"${JAVA_1_8_HOME}/java" -cp ".:./pipe_file_parser.jar:./ap_batches.jar" blog.javamagic.pfp.PfpRunner "${params}"
+	"${JAVA_1_8_HOME}/java" -cp "${PFP_HOME}:${PFP_HOME}/pipe_file_parser.jar" blog.javamagic.pfp.PfpRunner "${params}"
 	set +f
 }
